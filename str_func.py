d9ac364b93fe4dfae1082da8cb5460d586acc5d5
daren <titadarenka@gmail.com>
2023-06-12 17:00:44 +0300

    added func2, added docstring to func1

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_letter(text):
+    """делает текст закглавными буквами"""
     return str.upper(text)
+
+
+def capital_letter(text):
+    """Делает заглавными первые буквы каждого слова в строке"""
+    return text.title()
